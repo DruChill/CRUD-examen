39f7d694e0709fb62a149e26f543870d3c71f0c9
druchill <73948926@elp.edu.pe>
2025-04-26 15:26:57 -0500

    examen

---
+++ CRUD-MVC/Migrations/20250412190845_SeedCategory.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CRUD_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Coca Cola", 2 },
+                    { 2, "Inka Cola", 1 },
+                    { 3, "Fanta", 2 },
+                    { 4, "Sprite", 3 },
+                    { 5, "Coca", 4 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
---
+++ CRUD-MVC/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_MVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Nombre de categoria")]
+        public string Name { get; set; }
+        [DisplayName("Nombre de precio")]
+
+        public int Price { get; set; }
+    }
+}
